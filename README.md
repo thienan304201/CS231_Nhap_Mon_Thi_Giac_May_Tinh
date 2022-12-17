ae873d66e0b718bcea81156bff4c62ed892673ff
thienan304201 <19521186@gm.uit.edu.vn>
2022-12-18 00:19:44 +0700

    modify webcam cartoonize, add video cartoonize

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CS231_Nhap_Mon_Thi_Giac_May_Tinh
 
 Đồ án cuối kỳ
+
+Chuyển ảnh - video - webcam realtime thành hình ảnh hoạt hình
--- Webcam_Cartoonize.py
+++ Webcam_Cartoonize.py
@@ -1,39 +1,37 @@
 import cv2
-import numpy as np
-# Setup camera
-cap = cv2.VideoCapture(0)
-# Set a smaller resolution
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-# Just a dummy frame, will be overwritten
-last_foreground = np.zeros((480, 640), dtype='uint8')
-while True:
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+webcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+# Check if camera opened successfully
+if (webcam.isOpened() == False):
+    print("Error opening video stream or file")
+
+# Read the video
+while(webcam.isOpened()):
     # Capture frame-by-frame
-    _, frame = cap.read()
-    # Only needed if you webcam does not support 640x480
-    frame = cv2.resize(frame, (640, 480))
-    # Flip it to mirror you
-    frame = cv2.flip(frame, 1)
-    # Convert to gray scale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Keep the foreground
-    foreground = gray
-    # Take the absolute difference
-    abs_diff = cv2.absdiff(foreground, last_foreground)
-    # Update the last foreground image
-    last_foreground = foreground
-    _, mask = cv2.threshold(abs_diff, 20, 255, cv2.THRESH_BINARY)
-    mask = cv2.dilate(mask, None, iterations=3)
-    se = np.ones((85, 85), dtype='uint8')
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, se)
-    frame_effect = cv2.stylization(frame, sigma_s=150, sigma_r=0.25)
-    idx = (mask > 1)
-    frame[idx] = frame_effect[idx]
-    # cv2.imshow('WebCam (Mask)', mask)
-    cv2.imshow('WebCam (frame)', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv2.waitKey(0)
+    ret, frame = webcam.read()
+    if ret == True:
+        # Flip mirro
+        frame = cv2.flip(frame, 1)
+        color = cv2.bilateralFilter(frame, 9, 9, 7)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blur = cv2.medianBlur(gray, 7)
+        edges = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 2)
+        frame_edge = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+        cartoon = cv2.bitwise_and(color, frame_edge)
+        # Display the resulting frame
+        cv2.imshow('Cartoonized', cartoon)
+        # Press q on keyboard to  exit
+        if cv2.waitKey(25) == ord('q'):
+            break
+            # Press s on keyboard to save a screenshot
+        elif cv2.waitKey(25) == ord('s'):
+            cv2.imwrite('screenshot.png', cartoon)
+
+# When everything done, release the video capture object
+webcam.release()
+
+# Closes all the frames
 cv2.destroyAllWindows()
